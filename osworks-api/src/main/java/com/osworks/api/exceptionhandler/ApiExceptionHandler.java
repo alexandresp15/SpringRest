ac97635cd285c7a4888fb5f291efe86f06ea118e
Alexandre Pereira <alexandresp15@gmail.com>
2020-06-19 08:10:52 -0300

    Code refactoring and added external library

--- osworks-api/src/main/java/com/osworks/api/exceptionhandler/ApiExceptionHandler.java
+++ osworks-api/src/main/java/com/osworks/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.osworks.api.exceptionhandler;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 
 import org.springframework.http.HttpHeaders;
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.osworks.domain.exception.EntidadeNaoEncontradaException;
 import com.osworks.domain.exception.NegocioException;
 
 @ControllerAdvice
@@ -21,7 +22,20 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	
 //	@Autowired
 //	private MessageSource messageSource;
-	
+
+	@ExceptionHandler(EntidadeNaoEncontradaException.class)
+	public ResponseEntity<Object> hadleEntidadeNaoEncontrada(NegocioException ex, WebRequest request) {
+		var status = HttpStatus.NOT_FOUND;
+		
+		var problema = new Problema();
+		problema.setStatus(status.value());
+		problema.setTitulo(ex.getMessage());
+		problema.setDataHora(OffsetDateTime.now());
+		
+		return handleExceptionInternal(ex, problema, new HttpHeaders(), status, request); 
+		
+	}	
+
 	@ExceptionHandler(NegocioException.class)
 	public ResponseEntity<Object> hadleNegocio(NegocioException ex, WebRequest request) {
 		var status = HttpStatus.BAD_REQUEST;
@@ -29,7 +43,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		var problema = new Problema();
 		problema.setStatus(status.value());
 		problema.setTitulo(ex.getMessage());
-		problema.setDataHora(LocalDateTime.now());
+		problema.setDataHora(OffsetDateTime.now());
 		
 		return handleExceptionInternal(ex, problema, new HttpHeaders(), status, request); 
 		
@@ -52,7 +66,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		problema.setStatus(status.value());
 		problema.setTitulo("Um ou mais campos estão inválidos. "
 				+ "Faça o preechimento correto e tente novamente");
-		problema.setDataHora(LocalDateTime.now());
+		problema.setDataHora(OffsetDateTime.now());
 		problema.setCampos(campos);
 	
 		return super.handleExceptionInternal(ex, problema, headers, status, request);
